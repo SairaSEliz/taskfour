3272979fb75e1c7efd4ded9b03120ae7c6818400
SairaSEliz <sairaselizabeth20@gmail.com>
2024-08-11 02:30:24 -0400

    Delete hello_time_rust directory

--- hello_time_rust/src/main.rs
+++
@@ -1,8 +0,0 @@
-use chrono::Local;
-
-fn main() {
-    let name = "Saira S Elizabeth"; // Replace with your name
-    let current_time = Local::now();
-    println!("Hello {}, right now the time is {}", name, current_time.format("%Y-%m-%d %H:%M:%S"));
-}
-
--- hello_time_rust/target/debug/deps/hello_time-15946d1a6f29a475.d
+++
@@ -1,5 +0,0 @@
-/home/kali/taskfour/hello_time/target/debug/deps/hello_time-15946d1a6f29a475: src/main.rs
-
-/home/kali/taskfour/hello_time/target/debug/deps/hello_time-15946d1a6f29a475.d: src/main.rs
-
-src/main.rs:
